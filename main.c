8f80c35e6d6a3f58263d5f780e83f19fe2abe60e
Mohamed Ghaibouche <116889156+Sorbonnar@users.noreply.github.com>
2026-08-05 12:13:35 +0200

    fix conflicts

--- main.c
+++ main.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include "temperature_input.h"
+#include "alerte.h"
 
-int main(void)
-{
+int main(void) {
     /* Tableau qui stockera les températures */
-    float temperatures[MAX_RELEVES];
+    float temperatures[24] = {2.4, -3.2, 22, 25.2};
 
     /* Nombre de relevés saisis */
     int nb_releves;
+    Config cfg = {.seuil_chaud = 35.0, .seuil_froid = 0.0, .seuil_amplitude = 20.0}; // Valeur par defaut
 
     /* Appel de la fonction de saisie */
     saisir_releves(temperatures, &nb_releves);
 
-=======
-#include "alerte.h"
-
-#define N 4
-
-int main(void){
-
-    float tab[N] = {2.4, -3.2, 22, 25.2 };
-
-    Config cfg = { .seuil_chaud = 35.0, .seuil_froid = 0.0, .seuil_amplitude = 20.0 }; // Valeur par defaut
-
-    analyser_alertes(tab, N, &cfg);
-
->>>>>>> 73788ee12c714b4eeb6e35464ad6eb3b0745f482
+    analyser_alertes(temperatures, nb_releves, &cfg);
     return 0;
 }
