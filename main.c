df29da6cbe5c12ee163a1154b73aeb73d9765469
Yohan Cosnay <cosnay.yohan@orange.fr>
2026-08-05 12:06:29 +0200

    Ajout module de température de saisie

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include "temperature_input.h"
 
-int main(void){
+int main(void)
+{
+    /* Tableau qui stockera les températures */
+    float temperatures[MAX_RELEVES];
+
+    /* Nombre de relevés saisis */
+    int nb_releves;
+
+    /* Appel de la fonction de saisie */
+    saisir_releves(temperatures, &nb_releves);
+
     return 0;
 }
--- temperature_input.c
+++ temperature_input.c
@@ -1,9 +1,70 @@
 #include <stdio.h>
+#include "temperature_input.h"
 
-#define MAX_RELEVES 24
-#define TEMP_MIN -50.0
-#define TEMP_MAX 60.0
 
-int main(void) {
+/*
+* Fonction : saisir_releves
+* -------------------------
+* Demande à l'utilisateur de saisir un nombre de relevés
+* puis les températures correspondantes.
+*
+* Paramètres :
+*  - temperatures : tableau contenant les températures saisies
+*  - nb_releves   : pointeur vers le nombre de relevés
+*/
+void saisir_releves(float temperatures[], int *nb_releves){
 
+	/* Demande le nombre de relevés à l'utilisateur tant que le nombre n'est
+	   pas compris entre 1 et le nombre max de relevés (24)*/
+	do
+	{
+		printf("Nombre de releves (1-%d) : ", MAX_RELEVES);
+		if (scanf("%d", nb_releves) != 1)
+                {
+                        printf("Erreur : veuillez saisir un nombre.\n");
+                        /* Vide le reste de la ligne */
+                        while (getchar() != '\n');
+                        continue;
+                }
+
+		if (*nb_releves < 1 || *nb_releves > MAX_RELEVES)
+		{
+			printf("Nombres de relevés hors plage (1 - %d)\n", MAX_RELEVES);
+		}
+
+	} while(*nb_releves < 1 || *nb_releves > MAX_RELEVES);
+
+	/* Saisie de chaque température */
+	for( int i=0; i < *nb_releves; i++)
+	{
+		/* Température à l'heure i */
+		printf("Heure %02d : ",i);
+		if (scanf("%f", &temperatures[i]) != 1)
+    		{
+        		printf("Erreur : veuillez saisir un nombre.\n");
+        		/* Vide le reste de la ligne */
+        		while (getchar() != '\n');
+        		continue;
+    		}
+
+		/* Vérification de la température */
+		while(temperatures[i] < TEMP_MIN || temperatures[i] > TEMP_MAX)
+		{
+                	printf("Valeur hors plage [-50.0 ; 60.0]. Ressaisir : ");
+			if (scanf("%f", &temperatures[i]) != 1)
+			{
+   				printf("Erreur : veuillez saisir un nombre.\n");
+    				while(getchar() != '\n');
+    				continue;
+			}
+        	}
+    	}
+
+    	/* Affichage de tous les relevés enregistrés */
+    	printf("\n--- Releves enregistres ---\n");
+
+    	for(int i = 0; i < *nb_releves; i++)
+    	{
+        	printf("Heure %02d : %.1f °C\n", i, temperatures[i]);
+    	}
 }
