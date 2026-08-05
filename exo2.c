dab3ab9b2b7b62f81c145bcf7bef3ab2f15e90a9
Mohamed Ghaibouche <mohamed.f.ghaibouche@gmail.com>
2026-08-05 11:16:52 +0200

    moyenne

--- exo2.c
+++ exo2.c
@@ -1,5 +1,11 @@
 float calculer_moyenne(float *tab, int n) {
+    float somme = 0.0;
 
+    for (int i = 0; i < n; i++) {
+        somme += tab[i];
+    }
+
+    return somme / n;
 }
 
 float trouver_minimum(float *tab, int n, int *idx) {
@@ -15,6 +21,6 @@ float calculer_amplitude(float *tab, int n) {
 }
 
 void afficher_valeurs_exo2(float moyenne, float min, float max, float amplitude) {
-    
+
 }
 
