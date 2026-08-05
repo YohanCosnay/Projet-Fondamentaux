b070be587b2f88ea4a324f650cd4c5e4662bef8f
Theo <theogillot2@hotmail.fr>
2026-08-05 12:01:04 +0200

    create basique tab and config

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include "alerte.h"
+
+#define N 4
 
 int main(void){
+
+    float tab[N] = {2.4, -3.2, 22, 25.2 };
+
+    Config cfg = { .seuil_chaud = 35.0, .seuil_froid = 0.0, .seuil_amplitude = 20.0 }; // Valeur par defaut
+
+    analyser_alertes(tab, N, &cfg);
+
     return 0;
 }
