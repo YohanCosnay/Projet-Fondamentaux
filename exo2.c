c7396da6aa58b8645289a0577040e15229b58d9c
Mohamed Ghaibouche <mohamed.f.ghaibouche@gmail.com>
2026-08-05 11:26:35 +0200

    min, max, amplitude

--- exo2.c
+++ exo2.c
@@ -9,18 +9,36 @@ float calculer_moyenne(float *tab, int n) {
 }
 
 float trouver_minimum(float *tab, int n, int *idx) {
+    float minimum = tab[0];
+    *idx = 0;
 
+    for (int i = 1; i < n; i++) {
+        if (tab[i] < minimum) {
+            minimum = tab[i];
+            *idx = i;
+        }
+    }
+
+    return minimum;
 }
 
 float trouver_maximum(float *tab, int n, int *idx) {
+    float maximum = tab[0];
+    *idx = 0;
 
+    for (int i = 1; i < n; i++) {
+        if (tab[i] > maximum) {
+            maximum = tab[i];
+            *idx = i;
+        }
+    }
+
+    return maximum;
 }
 
 float calculer_amplitude(float *tab, int n) {
+    int *idx_min;
+    int *idx_max;
 
+    return trouver_maximum(tab, n, idx_max) - trouver_minimum(tab, n, idx_min);
 }
-
-void afficher_valeurs_exo2(float moyenne, float min, float max, float amplitude) {
-
-}
-
