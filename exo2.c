c4241bb1e6d3645e7895a0f63622b17bcee4ebe4
Mohamed <mohamed.f.ghaibouche@gmail.com>
2026-08-05 11:07:55 +0200

    commit primordial

---
+++ exo2.c
@@ -0,0 +1,20 @@
+float calculer_moyenne(float *tab, int n) {
+
+}
+
+float trouver_minimum(float *tab, int n, int *idx) {
+
+}
+
+float trouver_maximum(float *tab, int n, int *idx) {
+
+}
+
+float calculer_amplitude(float *tab, int n) {
+
+}
+
+void afficher_valeurs_exo2(float moyenne, float min, float max, float amplitude) {
+    
+}
+
